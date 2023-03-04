3127e0125e587a7e84eb530877a1f2f87effb9cb
ilgner becheleni <ilgner.gui@gmail.com>
2023-03-04 15:20:12 -0300

    portifolio

---
+++ script.js
@@ -0,0 +1,25 @@
+$("#darkMode").click(function (e) {
+
+$("body").toggleClass("darkMode");
+
+$("a").toggleClass("fontWhite");
+$("h2").toggleClass("fontWhite");
+$("h3").toggleClass("fontWhite");
+$("h5").toggleClass("fontWhite");
+$("p").toggleClass("fontWhite");
+$(".card-body").toggleClass("darkMode");
+
+$(".fa-moon").toggleClass("visually-hidden");
+$(".fa-sun").toggleClass("visually-hidden");
+$(".tech").toggleClass("bg-ligth");
+
+
+
+
+
+
+
+
+
+
+});
